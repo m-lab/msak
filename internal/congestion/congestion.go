ba46a9acf702b8b2a2aa169f2b98fb2b0d032177
Roberto D'Auria <roberto@measurementlab.net>
2022-12-15 16:46:57 +0100

    Update comments in congestion.go

--- internal/congestion/congestion.go
+++ internal/congestion/congestion.go
@@ -13,16 +13,19 @@ import (
 // ErrNoSupport indicates that this system does not support BBR.
 var ErrNoSupport = errors.New("TCP_CC_INFO not supported")
 
-// Enable sets the congestion control algorithm for |fp|.
+// Set sets the congestion control algorithm for the given socket to a
+// string value. It can fail if the requested cc algorithm is not available.
 func Set(fp *os.File, cc string) error {
 	return set(fp, cc)
 }
 
+// Get returns the congestion control algorithm set for the given socket's
+// file descriptor.
 func Get(fp *os.File) (string, error) {
 	return get(fp)
 }
 
-// GetBBRInfo obtains BBR info from |fp|.
+// GetBBRInfo obtains BBR info from fp.
 func GetBBRInfo(fp *os.File) (inetdiag.BBRInfo, error) {
 	return getMaxBandwidthAndMinRTT(fp)
 }
