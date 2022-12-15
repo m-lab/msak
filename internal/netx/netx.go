b6dc5d9a29d1495704b59636ba7dd7d7e7830620
Roberto D'Auria <roberto@measurementlab.net>
2022-12-15 17:47:06 +0100

    Add docstring for netx.GetFile

--- internal/netx/netx.go
+++ internal/netx/netx.go
@@ -7,6 +7,9 @@ import (
 	"os"
 )
 
+// GetFile returns the socket's file descriptor for the given net.Conn.
+// It currently supports net.TCPConn and tls.Conn only, and will return an
+// error if you call it with something else.
 func GetFile(conn net.Conn) (*os.File, error) {
 	switch t := conn.(type) {
 	case *net.TCPConn:
