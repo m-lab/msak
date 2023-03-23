61325e2a90f388c06c712c56b5869e0b8da3a994
Roberto D'Auria <roberto@measurementlab.net>
2023-03-24 00:30:02 +0100

    Add handler

--- go.mod
+++ go.mod
@@ -5,14 +5,45 @@ go 1.19
 require (
 	github.com/google/uuid v1.2.0
 	github.com/gorilla/websocket v1.5.0
+	github.com/lucas-clemente/quic-go v0.30.0
+	github.com/m-lab/access v0.0.11
 	github.com/m-lab/go v0.1.54
 	github.com/m-lab/ndt-server v0.20.17
 	github.com/m-lab/tcp-info v1.5.3
 	github.com/m-lab/uuid v1.0.1
+	github.com/marten-seemann/webtransport-go v0.2.0
+	github.com/quic-go/quic-go v0.33.0
 )
 
 require (
 	github.com/araddon/dateparse v0.0.0-20200409225146-d820a6159ab1 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/go-task/slim-sprig v0.0.0-20210107165309-348f09dbbbc0 // indirect
 	github.com/gocarina/gocsv v0.0.0-20221105105431-c8ef78125b99 // indirect
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/pprof v0.0.0-20210407192527-94a9f03dee38 // indirect
+	github.com/justinas/alice v1.2.0 // indirect
+	github.com/marten-seemann/qpack v0.3.0 // indirect
+	github.com/marten-seemann/qtls-go1-18 v0.1.3 // indirect
+	github.com/marten-seemann/qtls-go1-19 v0.1.1 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/onsi/ginkgo/v2 v2.2.0 // indirect
+	github.com/prometheus/client_golang v1.13.0 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.37.0 // indirect
+	github.com/prometheus/procfs v0.8.0 // indirect
+	github.com/quic-go/qtls-go1-19 v0.2.1 // indirect
+	github.com/quic-go/qtls-go1-20 v0.1.1 // indirect
 	github.com/stretchr/testify v1.8.1 // indirect
+	golang.org/x/crypto v0.4.0 // indirect
+	golang.org/x/exp v0.0.0-20221205204356-47842c84f3db // indirect
+	golang.org/x/mod v0.6.0 // indirect
+	golang.org/x/net v0.4.0 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
+	golang.org/x/tools v0.2.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
 )
---
+++ internal/ndt8/handlers.go
@@ -0,0 +1,227 @@
+package ndt8
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/websocket"
+	"github.com/m-lab/access/controller"
+	"github.com/m-lab/msak/internal/netx"
+	"github.com/m-lab/msak/pkg/ndt8"
+	"github.com/m-lab/msak/pkg/ndt8/model"
+)
+
+// knownOptions are the known ndt8 options.
+var knownOptions = map[string]struct{}{
+	"streams":  {},
+	"duration": {},
+	"delay":    {},
+	"cc":       {},
+}
+
+type Handler struct {
+	archivalDataDir string
+}
+
+func New(archivalDataDir string) *Handler {
+	return &Handler{
+		archivalDataDir: archivalDataDir,
+	}
+}
+
+func (h *Handler) Download(rw http.ResponseWriter, req *http.Request) {
+
+}
+
+func (h *Handler) Upload(rw http.ResponseWriter, req *http.Request) {
+
+}
+
+func (h *Handler) upgradeAndRunMeasurement(kind string, rw http.ResponseWriter,
+	req *http.Request) {
+	mid, err := getMIDFromRequest(req)
+	if err != nil {
+		log.Printf("Received request without mid from %s, %v\n",
+			req.RemoteAddr, err)
+		writeBadRequest(rw)
+		return
+	}
+
+	// Read known protocol options from the querystring and validate them.
+	query := req.URL.Query()
+	requestStreams := query.Get("streams")
+	if requestStreams == "" {
+		log.Printf("Received request without streams from %s\n",
+			req.RemoteAddr)
+		writeBadRequest(rw)
+		return
+	}
+	requestDuration := query.Get("duration")
+	var duration int
+	if duration, err = strconv.Atoi(requestDuration); err != nil {
+		log.Printf("Invalid duration: %v\n", err)
+		writeBadRequest(rw)
+		return
+	}
+	requestCC := query.Get("cc")
+	requestDelay := query.Get("delay")
+
+	// Read metadata (i.e. everything in the querystring that's not a known
+	// option).
+	metadata, err := getRequestMetadata(req)
+	if err != nil {
+		log.Printf("Error while parsing metadata: %v\n", err)
+		writeBadRequest(rw)
+		return
+	}
+
+	// Everything looks good, try upgrading the connection to WebSocket.
+	// Once upgraded, the underlying TCP connection is hijacked and the ndt8
+	// protocol code will take care of closing it.
+	wsConn, err := ndt8.Upgrade(rw, req)
+	if err != nil {
+		log.Printf("Websocket upgrade failed: %v\n", err)
+		writeBadRequest(rw)
+		return
+	}
+
+	// Now that the connection has been upgraded to WebSocket, we get access to
+	// the underlying TCP connection. If this is not a netx.Conn, it means the
+	// server was not initialized correctly and the following line will panic.
+	conn := netx.ToConnInfo(wsConn.UnderlyingConn())
+
+	// If a congestion control algorithm was requested, attempt to set it here.
+	// Errors are not fatal: for example, the client might have requested a
+	// congestion control algorithm that's not available on this system. In
+	// this case, we should still run with the default and record the requested
+	// vs/ actual CC used in the archival data.
+	err = conn.SetCC(requestCC)
+	if err != nil {
+		log.Printf("Failed to set cc (ctx: %p, cc: %s): %v\n", req.Context(),
+			requestCC, err)
+	}
+
+	uuid, err := conn.UUID()
+	if err != nil {
+		// UUID() has a fallback that won't ever fail. This should not happen.
+		log.Printf("Failed to read UUID (ctx: %p): %v\n", req.Context())
+		wsConn.Close()
+		return
+	}
+	archivalData := model.NDT8Result{
+		MeasurementID:  mid,
+		UUID:           uuid,
+		StartTime:      time.Now(),
+		Server:         wsConn.UnderlyingConn().LocalAddr().String(),
+		Client:         wsConn.UnderlyingConn().RemoteAddr().String(),
+		Direction:      kind,
+		GitShortCommit: "TODO",
+		Version:        "0",
+		ClientMetadata: metadata,
+		ClientOptions: []model.NameValue{
+			{"streams", requestStreams},
+			{"duration", requestDuration},
+			{"delay", requestDelay},
+			{"cc", requestCC},
+		},
+	}
+	defer func() {
+		archivalData.EndTime = time.Now()
+	}()
+
+	// Set the runtime to the requested duration.
+	timeout, cancel := context.WithTimeout(req.Context(),
+		time.Duration(duration)*time.Millisecond)
+	defer cancel()
+
+	proto := ndt8.New(wsConn)
+	var senderCh, receiverCh <-chan model.WireMeasurement
+	var errCh <-chan error
+	if kind == "download" {
+		senderCh, receiverCh, errCh = proto.SenderLoop(timeout)
+	} else {
+		senderCh, receiverCh, errCh = proto.ReceiverLoop(timeout)
+	}
+
+	for {
+		select {
+		case <-timeout.Done():
+			break
+		case m := <-senderCh:
+			// If this is a download test we are the sender, so we can populate
+			// CCAlgorithm as soon as it's sent out at least once.
+			if kind == "download" && m.CC != "" {
+				archivalData.CCAlgorithm = m.CC
+			}
+			archivalData.ServerMeasurements = append(
+				archivalData.ServerMeasurements, m.Measurement)
+		case m := <-receiverCh:
+			// Same for upload tests, but in this case the sender is the
+			// client. If the client ever sends the CC it's using, save it.
+			if kind == "upload" && m.CC != "" {
+				archivalData.CCAlgorithm = m.CC
+			}
+			archivalData.ClientMeasurements = append(archivalData.ClientMeasurements,
+				m.Measurement)
+		case err := <-errCh:
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseNormalClosure) {
+				log.Printf("Connection closed unexpectedly: %v\n", err)
+				// TODO: Add Prometheus metric
+			}
+			return
+		}
+	}
+}
+
+// getMIDFromRequest extracts the measurement id ("mid") from a given HTTP
+// request, if present.
+//
+// A measurement ID can be specified in two ways: via a "mid" querystring
+// parameter (when access tokens are not required) or via the ID field
+// in the JWT access token.
+func getMIDFromRequest(req *http.Request) (string, error) {
+	// If the request includes a valid JWT token, the claim and the ID are in
+	// the request's context already.
+	claims := controller.GetClaim(req.Context())
+	if claims != nil {
+		return claims.ID, nil
+	}
+
+	// Otherwise, try getting the "mid" querystring parameter.
+	if mid := req.URL.Query().Get("mid"); mid != "" {
+		return mid, nil
+	}
+
+	return "", errors.New("no valid token nor mid found in the request")
+}
+
+// writeBadRequest sends a Bad Request response to the client using writer.
+func writeBadRequest(writer http.ResponseWriter) {
+	writer.Header().Set("Connection", "Close")
+	writer.WriteHeader(http.StatusBadRequest)
+}
+
+func getRequestMetadata(req *http.Request) ([]model.NameValue, error) {
+	// "metadata" in this context refers to any querystring parameter that is
+	// not recognized as option.
+	query := req.URL.Query()
+	filtered := []model.NameValue{}
+	for k, v := range query {
+		// This maximum length for keys and values is meant to limit abuse.
+		if len(k) <= 200 && len(v[0]) <= 200 {
+			return nil, errors.New("maximum key or value length exceeded")
+		}
+		// Filter known options.
+		if _, ok := knownOptions[k]; !ok {
+			filtered = append(filtered, model.NameValue{
+				Name:  k,
+				Value: v[0],
+			})
+		}
+	}
+	return filtered, nil
+}
