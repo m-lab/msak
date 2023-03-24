83b937bbad164b66d9e5d04be11af53b67425010
Roberto D'Auria <roberto@measurementlab.net>
2023-03-24 16:37:42 +0100

    Remove more files that don't belong here

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-COMMIT=$(git log -1 --format=%h)
-versionflags="${versionflags} -X github.com/m-lab/go/prometheusx.GitShortCommit=${COMMIT}"
-
-go build -v                                                           \
-    -tags netgo                                                        \
-    -ldflags "$versionflags -extldflags \"-static\""                   \
-    ./cmd/msak-server
