1fa18bdc504506c9711a0b23670fd02ecc71afd1
Roberto D'Auria <roberto@measurementlab.net>
2023-03-24 16:30:14 +0100

    Add script to generate local test certs

--- gen_local_test_certs.bash
+++ gen_local_test_certs.bash
@@ -3,5 +3,5 @@
 set -euxo pipefail
 
 openssl genrsa -out key.pem
-openssl req -new -x509 -key key.pem -out cert.pem -days 2 -subj "/C=XX/ST=State/L=Locality/O=Org/OU=Unit/CN=localhost/emailAddress=test@email.address"
+openssl req -new -x509 -key key.pem -out cert.pem -days 2 -subj "/C=XX/ST=State/L=Locality/O=Org/OU=Unit/CN=localhost/emailAddress=test@email.address" -addext "subjectAltName = DNS:localhost"
 mv key.pem cert.pem certs/
