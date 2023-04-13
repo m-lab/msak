88337bf2bb63e2860a23c102e70b78170907ccb3
Roberto D'Auria <roberto@measurementlab.net>
2023-04-13 17:15:39 +0200

    Remove extra white space

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 COMMIT=$(git log -1 --format=%h)
 versionflags="-X github.com/m-lab/go/prometheusx.GitShortCommit=${COMMIT}"
 
-go build -v                                                           \
-    -tags netgo                                                        \
-    -ldflags "$versionflags -extldflags \"-static\""                   \
+go build -v                                          \
+    -tags netgo                                      \
+    -ldflags "$versionflags -extldflags \"-static\"" \
     ./cmd/msak-server
