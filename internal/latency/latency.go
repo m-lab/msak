b27425a55254d0ee21e2af21420bad6e4f460694
Roberto D'Auria <roberto@measurementlab.net>
2023-06-23 14:08:10 +0200

    s/network/transport/ and s/src/addr

--- internal/latency/latency.go
+++ internal/latency/latency.go
@@ -49,7 +49,7 @@ func NewHandler(dir string, cacheTTL time.Duration) *Handler {
 		// Save data to disk when the session expires.
 		archive := i.Value().Archive()
 		archive.EndTime = time.Now()
-		_, err := persistence.WriteDataFile(dir, "latency", "network", archive.ID, archive)
+		_, err := persistence.WriteDataFile(dir, "latency", "transport", archive.ID, archive)
 		if err != nil {
 			log.Error("failed to write latency result", "mid", archive.ID, "error", err)
 			return
@@ -294,7 +294,7 @@ func (h *Handler) ProcessPacketLoop(conn net.PacketConn) {
 		if err != nil {
 			log.Debug("failed to process packet",
 				"err", err,
-				"src", addr.String())
+				"addr", addr.String())
 		}
 	}
 }
