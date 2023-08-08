4b9f1d773152ac0d5783e6c8dec23cb100d94e72
Roberto D'Auria <roberto@measurementlab.net>
2023-08-09 00:33:47 +0200

    Set default value of version.Version to "unspecified".

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,4 +2,4 @@ package version
 
 // Version is the version of msak. This is meant to be overridden at compile
 // time using `-ldflags "-X var=value`
-var Version string
+var Version = "unspecified"
