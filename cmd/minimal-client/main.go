850f1984a787b392daca8c641eeae08fb69986f9
Stephen Soltesz <stephen.soltesz@gmail.com>
2024-01-04 18:29:45 -0500

    Remove other m-lab dependencies

--- cmd/minimal-client/main.go
+++ cmd/minimal-client/main.go
@@ -9,22 +9,22 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"net/url"
+	"path"
 	"runtime"
 	"time"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
-	"github.com/m-lab/go/flagx"
-	"github.com/m-lab/go/rtx"
-	"github.com/m-lab/locate/api/locate"
-	"github.com/m-lab/msak/pkg/throughput1/model"
 )
 
 const clientName = "msak-minimal-client-go"
 
 var clientVersion = "v0.0.1"
+
+const locateURL = "https://locate.measurementlab.net/v2/nearest/"
 
 var (
 	flagCC        = flag.String("cc", "bbr", "Congestion control algorithm to use")
@@ -32,12 +32,56 @@ var (
 	flagScheme    = flag.String("scheme", "wss", "Websocket scheme (wss or ws)")
 	flagNoVerify  = flag.Bool("no-verify", false, "Skip TLS certificate verification")
 	flagByteLimit = flag.Int("bytes", 0, "Byte limit to request to the server")
-	flagServer    = flagx.URL{}
+	flagServerURL = flag.String("server.url", "", "URL to directly target")
 	flagMID       = flag.String("mid", uuid.NewString(), "Measurement ID to use")
+	flagLocateURL = flag.String("locate.url", locateURL, "The base url for the Locate API")
 )
 
-func init() {
-	flag.Var(&flagServer, "server.url", "URL to directly target")
+// WireMeasurement is a wrapper for Measurement structs that contains
+// information about this TCP stream that does not need to be sent every time.
+// Every field except for Measurement is only expected to be non-empty once.
+type WireMeasurement struct {
+	// CC is the congestion control used by the sender of this WireMeasurement.
+	CC string `json:",omitempty"`
+	// UUID is the unique identifier for this TCP stream.
+	UUID string `json:",omitempty"`
+	// LocalAddr is the local TCP endpoint (ip:port).
+	LocalAddr string `json:",omitempty"`
+	// RemoteAddr is the server's TCP endpoint (ip:port).
+	RemoteAddr string `json:",omitempty"`
+	// Measurement is the Measurement struct wrapped by this WireMeasurement.
+	Measurement
+}
+
+// The Measurement struct contains measurement results. This structure is
+// meant to be serialised as JSON and sent as a textual message.
+type Measurement struct {
+	// Application contains the application-level BytesSent/Received pair.
+	Application ByteCounters
+	// Network contains the network-level BytesSent/Received pair.
+	Network ByteCounters
+
+	// ElapsedTime is the time elapsed since the start of the measurement
+	// according to the party sending this Measurement.
+	ElapsedTime int64 `json:",omitempty"`
+
+	// BBRInfo is an optional struct containing BBR metrics. Only applicable
+	// when the congestion control algorithm used by the party sending this
+	// Measurement is BBR.
+	BBRInfo map[string]int64 `json:",omitempty"`
+
+	// TCPInfo is an optional struct containing some of the TCP_INFO kernel
+	// metrics for this TCP stream. Only applicable when the party sending this
+	// Measurement has access to it.
+	TCPInfo map[string]int64 `json:",omitempty"`
+}
+
+type ByteCounters struct {
+	// BytesSent is the number of bytes sent.
+	BytesSent int64 `json:",omitempty"`
+
+	// BytesReceived is the number of bytes received.
+	BytesReceived int64 `json:",omitempty"`
 }
 
 // localDialer allows insecure TLS for explicit servers.
@@ -70,30 +114,80 @@ func connect(ctx context.Context, s *url.URL) (*websocket.Conn, error) {
 }
 
 // formatMessage reports a WireMeasurement in a human readable format.
-func formatMessage(prefix string, stream int, m model.WireMeasurement) {
-	fmt.Printf("%s #%d - avg %0.2f Mbps, elapsed %0.4fs, payload r/w: %d/%d, network r/w: %d/%d kernel* r/w: %d/%d\n",
+func formatMessage(prefix string, stream int, m WireMeasurement) {
+	log.Printf("%s #%d - avg %0.2f Mbps, elapsed %0.4fs, payload r/w: %d/%d, network r/w: %d/%d kernel* r/w: %d/%d\n",
 		prefix, stream,
 		8*float64(m.Network.BytesSent)/(float64(m.ElapsedTime)),
 		float64(m.ElapsedTime)/1000000.0,
 		m.Application.BytesReceived, m.Application.BytesSent,
 		m.Network.BytesReceived, m.Network.BytesSent,
-		m.TCPInfo.BytesReceived, m.TCPInfo.BytesSent,
+		m.TCPInfo["BytesReceived"], m.TCPInfo["BytesSent"],
 	)
+}
+
+// Target is returned by the Locate API.
+type Target struct {
+	// URLs contains measurement service resource names and the complete URL for
+	// running a measurement.
+	URLs map[string]string `json:"urls"`
+}
+
+// NearestResult is returned by the location service in response to query
+// requests.
+type NearestResult struct {
+	// Results contains an array of Targets matching the client request.
+	Results []Target `json:"results,omitempty"`
+}
+
+func locateServers(ctx context.Context, userAgent, locate string) ([]Target, error) {
+	u, err := url.Parse(*flagLocateURL)
+	if err != nil {
+		return nil, err
+	}
+	u.Path = path.Join(u.Path, locate)
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, u.String(), nil)
+	if err != nil {
+		// e.g. due to an invalid parameter.
+		return nil, err
+	}
+	if userAgent == "" {
+		// user agent is required.
+		return nil, errors.New("no user agent given")
+	}
+	req.Header.Set("User-Agent", userAgent)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	reply := &NearestResult{}
+	err = json.Unmarshal(b, reply)
+	if err != nil {
+		return nil, err
+	}
+	return reply.Results, err
 }
 
 // getDownloadServer find a single server from given flags or Locate API.
 func getDownloadServer(ctx context.Context) (*url.URL, error) {
 	// Use explicit server if provided.
-	if flagServer.URL != nil {
-		q := flagServer.URL.Query()
+	if *flagServerURL != "" {
+		u, err := url.Parse(*flagServerURL)
+		if err != nil {
+			return nil, err
+		}
+		q := u.Query()
 		q.Set("mid", *flagMID)
-		flagServer.URL.RawQuery = q.Encode()
-		return flagServer.URL, nil
+		u.RawQuery = q.Encode()
+		return u, nil
 	}
 
 	// Use Locate API to request otherwise.
-	lc := locate.NewClient(clientName + "/" + clientVersion)
-	targets, err := lc.Nearest(ctx, "msak/throughput1")
+	targets, err := locateServers(ctx, clientName+"/"+clientVersion, "msak/throughput1")
 	if err != nil {
 		return nil, err
 	}
@@ -123,7 +217,9 @@ func main() {
 	defer cancel()
 
 	conn, err := getConn(ctx)
-	rtx.Must(err, "failed to get websocket.Conn to server")
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer conn.Close()
 
 	// Max runtime.
@@ -141,7 +237,7 @@ func main() {
 			kind, reader, err := conn.NextReader()
 			if err != nil {
 				if !websocket.IsCloseError(err, websocket.CloseNormalClosure) {
-					fmt.Println("error", err)
+					log.Println("error", err)
 				}
 				return
 			}
@@ -150,21 +246,21 @@ func main() {
 				// Binary messages are discarded after reading their size.
 				size, err := io.Copy(io.Discard, reader)
 				if err != nil {
-					fmt.Println("error", err)
+					log.Println("error", err)
 					return
 				}
 				applicationBytesReceived += size
 			case websocket.TextMessage:
 				data, err := io.ReadAll(reader)
 				if err != nil {
-					fmt.Println("error", err)
+					log.Println("error", err)
 					return
 				}
 				applicationBytesReceived += int64(len(data))
 
-				var m model.WireMeasurement
+				var m WireMeasurement
 				if err := json.Unmarshal(data, &m); err != nil {
-					fmt.Println("error", err)
+					log.Println("error", err)
 					return
 				}
 				formatMessage("Server", 1, m)
